21e1bc495077fb53f0f903a0290fe0885c82306e
Mouna Cheikhna <cheikhnamouna@gmail.com>
2016-12-29 10:21:10 +0100

    solution for ex2 of mind cipher.

--- src/MindCipher.hs
+++ src/MindCipher.hs
@@ -14,12 +14,22 @@ flipUntil p = fmap (length . takeWhile (not . isPrefixOf p) . tails . randomRs (
 day :: [Bool] -> IO Double
 day p = fmap (\a -> fromIntegral (sum a) / fromIntegral (length a)) . replicateM 10000 $ flipUntil p
 
-mondayFlipNumber = day [True, False, True]
-
-tuesdayFlipNumber = day [True, False, False]
-
+-- mondayFlipNumber = day [True, False, True]
+-- tuesdayFlipNumber = day [True, False, False]
 -- results show that monday's number is higher
 
 -- | Improvement 2 to day with pointfree style
+day_2 :: [Bool] -> IO Double
 day_2 p = fmap (ap ((/) . fromIntegral . sum) (fromIntegral . length)) . replicateM 10000 $ flipUntil p
 
+-- | ex 2
+--  find the first year for which the sum of both groups of two digits is equal to the middle two digits.
+sumDay :: Maybe Integer
+sumDay = find (\d -> div d 100 + mod d 100 == div (mod d 1000) 10) [1979..]
+
+-- result is 2307
+
+main :: IO ()
+main = do print =<< liftM2 compare (day [True, False, True])
+                                   (day [True, False, False])
+          print sumDay
