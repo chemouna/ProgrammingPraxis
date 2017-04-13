46c45eceb68104ab2b39ea97fbaf1075615ee185
Mouna Cheikhna <cheikhnamouna@gmail.com>
2017-04-13 07:38:47 +0200

    Add pandigital check

--- src/PandigitalNumbers.hs
+++ src/PandigitalNumbers.hs
@@ -2,12 +2,18 @@
 module PandigitalNumbers where
 
 import Data.List
+-- import Control.Lens
 
 pandigital :: [(Int, Int, Int)]
 pandigital = [(a, b, a+b) | a <- d3, b <- d3, a+b > 900, b > a, a+b > 999, unique [a, b, a+b]]
   where d3 = filter (unique . return) [100..999]
         unique = (\x -> x == nub x) . (show =<<)
 
---smallestPandigital :: Int
---smallestPandigital = first $ head pandigital
+smallestPandigital :: Int
+smallestPandigital = (head pandigital) ^. _1
 
+main :: IO ()
+main = do print $ head pandigital == (246,789,1035) && smallestPandigital == 246
+          print pandigital
+
+          
