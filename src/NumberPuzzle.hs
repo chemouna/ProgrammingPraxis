303fc6c363ef0132105b242cde3b05f280b8fc89
Mouna Cheikhna <cheikhnamouna@gmail.com>
2017-04-13 22:03:27 +0200

    Adding some logs

--- src/NumberPuzzle.hs
+++ src/NumberPuzzle.hs
@@ -13,15 +13,19 @@ solverec :: Int -> Int -> Int -> [Int] -> [Int]
 solverec b n q s
     | n == 11 = [q]
     | otherwise =
-        trace (" -- q: " ++ show q) $
-        trace (" --- n:  " ++ show n)
+        trace "-----------------------------------"
+        trace (" q: " ++ show q) $
+        trace (" n:  " ++ show n)
         concatMap
         (\x ->
             let j = b * q + x in
-               trace (" ---- j: " ++ show j) $
+               trace ("  j: " ++ show j) $
                if j `mod` n /= 0 || x `elem` s
-               then []
-               else solverec b (n + 1) j (x:s))
+               then trace " we found case where not divisible by n so ignoring it and going to next value of gen"
+                 []
+               else
+                 trace "we found case where div by n so lets do it for the rest"
+                 solverec b (n + 1) j (x:s))
         (gen n)
 
 solve :: Int -> [Int]
