12033e0fd0aaae45cf50a6c3aca0e96c540449e1
Mouna Cheikhna <cheikhnamouna@gmail.com>
2017-04-13 07:40:13 +0200

    Finish Pandigital solution.

--- src/PandigitalNumbers.hs
+++ src/PandigitalNumbers.hs
@@ -1,8 +1,7 @@
-
 module PandigitalNumbers where
 
 import Data.List
--- import Control.Lens
+import Control.Lens
 
 pandigital :: [(Int, Int, Int)]
 pandigital = [(a, b, a+b) | a <- d3, b <- d3, a+b > 900, b > a, a+b > 999, unique [a, b, a+b]]
@@ -10,10 +9,8 @@ pandigital = [(a, b, a+b) | a <- d3, b <- d3, a+b > 900, b > a, a+b > 999, uniqu
         unique = (\x -> x == nub x) . (show =<<)
 
 smallestPandigital :: Int
-smallestPandigital = (head pandigital) ^. _1
+smallestPandigital = head pandigital ^. _1
 
 main :: IO ()
 main = do print $ head pandigital == (246,789,1035) && smallestPandigital == 246
           print pandigital
-
-          
