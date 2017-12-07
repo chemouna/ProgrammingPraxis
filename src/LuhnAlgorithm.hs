c02f3458a1621fa3d347338a40a5f4c8f8b3e67d
Mouna Cheikhna <cheikhnamouna@gmail.com>
2017-12-07 08:00:45 +0000

    Luhn and start of 7 bridges problem.

---
+++ src/LuhnAlgorithm.hs
@@ -0,0 +1,8 @@
+
+module LuhnAlgorithm where
+
+import Data.Char
+import Data.List.HT
+import Test.QuickCheck
+
+
---
+++ src/SevenBridgesKonigsberg.hs
@@ -0,0 +1,10 @@
+
+module SevenBridgesKonigsberg where
+
+import Data.List
+import qualified Data.Map as M
+
+-- A graph that has 1 or more than 2 vertices with an odd amount of neighbours will never be a eulerian path.
+
+
+
